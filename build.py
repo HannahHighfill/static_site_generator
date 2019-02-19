7a7d27ae01c9d656dc44902f9db1a312e337b742
Hannah Highfill <hannahhighfill@gmail.com>
2019-02-18 17:58:43 -0800

    sending up files

--- build.py
+++ build.py
@@ -1,6 +1,20 @@
 print('Building site!')
 top_html = open('templates/top.html'.read()
 middle_html = open('content/index.html').read()
-bottom_html=open('templates/bottom.html').read()
+bottom_html = open('templates/bottom.html').read()
 combined_html = top_html +middle_html + bottom_html
-open('output_file.html',w+10.write(combined.html)
+open('docs/output_file.html',"w+").write(combined_html)
+
+#print('Building site!')
+#top_html = open('templates/top.html'.read()
+#middle_html = open('content/index.html').read()
+#bottom_html=open('templates/bottom.html').read()
+#combined_html = top_html +middle_html + bottom_html
+#open('docs/output_file.html',"w+").write(combined_html)
+
+#print('Building site!')
+#top_html = open('templates/top.html'.read()
+#middle_html = open('content/index.html').read()
+#bottom_html=open('templates/bottom.html').read()
+#combined_html = top_html +middle_html + bottom_html
+#open('docs/output_file.html',"w+").write(combined_html)
