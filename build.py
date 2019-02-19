ff37c46a25d1a7eea223e27999098e327ae9480a
Hannah Highfill <hannahhighfill@gmail.com>
2019-02-18 18:17:15 -0800

    got pages working, still no top bar

--- build.py
+++ build.py
@@ -1,20 +1,20 @@
-print('Building site!')
-top_html = open('templates/top.html'.read()
-middle_html = open('/content/index.html').read()
+print('Building Home page')
+top_html = open('templates/top.html').read()
+middle_html = open('content/index.html').read()
 bottom_html = open('templates/bottom.html').read()
 combined_html = top_html + middle_html + bottom_html
 open('docs/output_file.html',"w+").write(combined_html)
 
-#print('Building site!')
-#top_html = open('templates/top.html'.read()
-#middle_html = open('content/blog.html').read()
-#bottom_html=open('templates/bottom.html').read()
-#combined_html = top_html +middle_html + bottom_html
-#open('docs/output_file.html',"w+").write(combined_html)
+print('Building Blog page')
+top_html = open('templates/top.html').read()
+middle_html = open('content/blog.html').read()
+bottom_html=open('templates/bottom.html').read()
+combined_html = top_html +middle_html + bottom_html
+open('docs/output_file2.html',"w+").write(combined_html)
 
-#print('Building site!')
-#top_html = open('templates/top.html'.read()
-#middle_html = open('content/projects.html').read()
-#bottom_html=open('templates/bottom.html').read()
-#combined_html = top_html +middle_html + bottom_html
-#open('docs/output_file.html',"w+").write(combined_html)
+print('Building projects page')
+top_html = open('templates/top.html').read()
+middle_html = open('content/projects.html').read()
+bottom_html=open('templates/bottom.html').read()
+combined_html = top_html +middle_html + bottom_html
+open('docs/output_file3.html',"w+").write(combined_html)
