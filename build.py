4710d19b034964066a9bf3de1589684e203749fb
Hannah Highfill <hannahhighfill@gmail.com>
2019-02-18 18:03:10 -0800

    changes to build python code

--- build.py
+++ build.py
@@ -1,20 +1,20 @@
 print('Building site!')
 top_html = open('templates/top.html'.read()
-middle_html = open('content/index.html').read()
+middle_html = open('/content/index.html').read()
 bottom_html = open('templates/bottom.html').read()
-combined_html = top_html +middle_html + bottom_html
+combined_html = top_html + middle_html + bottom_html
 open('docs/output_file.html',"w+").write(combined_html)
 
 #print('Building site!')
 #top_html = open('templates/top.html'.read()
-#middle_html = open('content/index.html').read()
+#middle_html = open('content/blog.html').read()
 #bottom_html=open('templates/bottom.html').read()
 #combined_html = top_html +middle_html + bottom_html
 #open('docs/output_file.html',"w+").write(combined_html)
 
 #print('Building site!')
 #top_html = open('templates/top.html'.read()
-#middle_html = open('content/index.html').read()
+#middle_html = open('content/projects.html').read()
 #bottom_html=open('templates/bottom.html').read()
 #combined_html = top_html +middle_html + bottom_html
 #open('docs/output_file.html',"w+").write(combined_html)
