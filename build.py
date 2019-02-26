08577e7f9bb91df28a6a2451c7d90b4e20c356f4
Hannah Highfill <hannahhighfill@gmail.com>
2019-02-25 18:22:22 -0800

    add index

--- build.py
+++ build.py
@@ -1,6 +1,20 @@
-print('Building site!')
-top_html = open('templates/top.html'.read()
-middle_html = open('content/index.html').read()
+print('Building Home page')
+top_html = open('templates/top.html').read()
+middle_html = open('content/home.html').read()
+bottom_html = open('templates/bottom.html').read()
+combined_html = top_html + middle_html + bottom_html
+open('docs/index.html',"w+").write(combined_html)
+
+print('Building Blog page')
+top_html = open('templates/top.html').read()
+middle_html = open('content/blog.html').read()
 bottom_html=open('templates/bottom.html').read()
 combined_html = top_html +middle_html + bottom_html
-open('output_file.html',w+10.write(combined.html)
+open('docs/blog.html',"w+").write(combined_html)
+
+print('Building projects page')
+top_html = open('templates/top.html').read()
+middle_html = open('content/projects.html').read()
+bottom_html=open('templates/bottom.html').read()
+combined_html = top_html +middle_html + bottom_html
+open('docs/projects.html',"w+").write(combined_html)
